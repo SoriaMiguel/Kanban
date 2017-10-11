f98d57047c7aac61535e216efcae7821c3556721
SoriaMiguel <soria.miguel.8@gmail.com>
2017-10-11 13:18:01 -0400

    pushing state to app

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
-
 import uuid from 'uuid';
-
 import Notes from './Notes';
 
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
   }
-];
+  render() {
+    const {notes} = this.state;
 
+    return (
+      <div>
+        <button onClick={() => console.log('add note')}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
+}
 
-export default () => (
-  <div>
-    <button onClick={() => console.log('add note')}>+</button>
+//After this change App owns the state even though the application
+//still should look the same as before. We can begin to use React's API
+//to modify the state.
 
-
-
-    <Notes notes={notes} />
-
-  </div>
-);
+//We're passing props to super by convention. If you don't pass
+//it, this.props won't get set! Calling super invokes the same method
+//of the parent class
