8c5a5250cf519e23f93f4f2a5aca43d628bc435b
SoriaMiguel <soria.miguel.8@gmail.com>
2017-10-12 13:28:11 -0400

    styling individual Notes

--- app/components/Editable.jsx
+++ app/components/Editable.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-export default ({editing, value, onEdit, ...props}) => {
+import classnames from 'classnames';
+
+export default ({editing, value, onEdit, className, ...props}) => {
   if(editing) {
-    return <Edit value={value} onEdit={onEdit} {...props} />;
+    return <Edit
+      className={className}
+      value={value}
+      onEdit={onEdit}
+      {...props} />;
   }
 
-  return <span {...props}>{value}</span>;
+  return <span className={classnames('value', className)}
+    {...props}>
+    {value}
+  </span>;
 }
 
 
@@ -13,7 +22,7 @@ export default ({editing, value, onEdit, ...props}) => {
 
 class Edit extends React.Component {
   render() {
-    const {value, onEdit, ...props} = this.props;
+    const {clasName, value, onEdit, ...props} = this.props;
 
     return <input
       type="text"
