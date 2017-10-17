44fd10d3e8cb74b5eabe2ac5a5fb7b765e78f24c
SoriaMiguel <soria.miguel.8@gmail.com>
2017-10-17 11:00:28 -0400

    connecting NoteActions with NoteStore: app/stores/NoteStore.js, tweaked store to connect the actions and to add initial stubs for logic

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid';
+import NoteActions from '../actions/NoteActions';
 
 export default class NoteStore {
   constructor() {
@@ -13,4 +14,15 @@ export default class NoteStore {
       }
     ];
   }
+
+  create(note) {
+    console.log('create note', note);
+  }
+  update(updatedNote) {
+    console.log('update note', updatedNote);
+  }
+  delete(id) {
+    console.log('delete note', id);
+  }
+
 }
